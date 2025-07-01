e1fb59be05b3157f825c9d58d3083be28d575fcf
kiran-mern <kiranprakash970@gmail.com>
2025-07-01 18:47:38 +0530

    removing mock data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 
 
 const Body = () => {
-    const[listOfRestraunt,setListOfRestraunt]=useState(resList);
+    const[listOfRestraunt,setListOfRestraunt]=useState([]);
+
+    useEffect(()=>{
+      fetchData();
+
+    },[])
+
+
+    const fetchData=async()=>{
+
+      const data=await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      
+    )
+
+      const json=await data.json();
+      console.log(json.data.cards,'aaa');
+      setListOfRestraunt(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+      
+    }
+
+    
   return (
     <div className="body">
       <div className="filter">
